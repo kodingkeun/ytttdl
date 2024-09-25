224b12bdfd291bb33cb5c7f5e71b34bd37d411d9
Dafi <dafinmaulana18@gmail.com>
2024-09-25 19:20:31 +0700

    feat: use layout and add seo meta

---
+++ app.vue
@@ -0,0 +1,21 @@
+<script setup>
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Youtube & Tiktok Downloader` : 'Youtube & Tiktok Downloader';
+  }
+});
+const description = 'Free Youtube and Tiktok video downloader. Download videos from Youtube and Tiktok to your phone or computer without any restrictions. Available on English and Bahasa Indonesia.';
+const keywords = 'youtube, tiktok, downloader, video, download, free, english, bahasa indonesia, youtubedl, tiktokdl, youtube downloader, tiktok downloader, download youtube video, download tiktok video, free youtube downloader, free tiktok downloader';
+
+useSeoMeta({
+  description,
+  keywords,
+  ogDescription: description
+});
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
