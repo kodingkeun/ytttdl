bf5e4dca023442a6d5a8d00fdeaa84390335ef8c
ibnusyawall <website.isas.24@gmail.com>
2024-10-06 18:24:34 +0700

    feat(nuxt.config.ts): add dotenv module to load environment variables
    feat(package.json): add @nuxtjs/dotenv as a dev dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: false },
     modules: ["@nuxtjs/tailwindcss", "@nuxt/fonts"],
+    buildModules: ["@nuxtjs/dotenv"],
     tailwindcss: {
         config: {
             darkMode: "selector",
